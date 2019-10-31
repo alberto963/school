9f3ff60c649fe36184207e232df50227dc716e32
Alberto <petazzi63@gmail.com>
2019-10-31 10:29:03 +0100

    hr_01: chg

--- hr_01_hiker/src/hr_01_hiker/Solution.java
+++ hr_01_hiker/src/hr_01_hiker/Solution.java
@@ -9,12 +9,16 @@ public class Solution {
 	static int countingValleys(int n, String s) {
 
 //		List<Character> asList = s.chars().mapToObj(c -> (char) c).collect(Collectors.toList());
-//
 //		List<Integer> l = asList.stream().map(v -> v == 'U' ? 1 : -1).collect(Collectors.toList());
 
 		List<Integer> l = s.chars().mapToObj(c -> c == 'U' ? 1 : -1).collect(Collectors.toList());
 
 		// System.out.println(l);
+		
+		List<Integer> sl = l.subList(0,  n);
+		int r = sl.stream().reduce(0, (a, v) -> a + v);
+		// TODO: accumulator should keep two values, prev and curr -> investigate how to do
+		
 		int i, h0 = 0, h = 0, c = 0;
 		for (i = 0; i < n; i++) {
 			h0 = h;
