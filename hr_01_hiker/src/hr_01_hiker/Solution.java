c78dc1f23fdcb64ead929421d016a1bd10909837
Alberto <petazzi63@gmail.com>
2019-10-31 09:55:00 +0100

    hr_01: chg

--- hr_01_hiker/src/hr_01_hiker/Solution.java
+++ hr_01_hiker/src/hr_01_hiker/Solution.java
@@ -9,11 +9,13 @@ public class Solution {
 	// Complete the countingValleys function below.
 	static int countingValleys(int n, String s) {
 
-		List<Character> asList = s.chars().mapToObj(c -> (char) c).collect(Collectors.toList());
+//		List<Character> asList = s.chars().mapToObj(c -> (char) c).collect(Collectors.toList());
+//
+//		List<Integer> l = asList.stream().map(v -> v == 'U' ? 1 : -1).collect(Collectors.toList());
 
-		List<Integer> l = asList.stream().map(v -> v == 'U' ? 1 : -1).collect(Collectors.toList());
+		List<Integer> l = s.chars().mapToObj(c -> c == 'U' ? 1 : -1).collect(Collectors.toList());
 
-		// System.out.println(l);
+		System.out.println(l);
 		int i, h0 = 0, h = 0, c = 0;
 		for (i = 0; i < n; i++) {
 			h0 = h;
