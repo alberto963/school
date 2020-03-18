126c49329d263669854d9ae5fd321b5988e507d5
Alberto <petazzi63@gmail.com>
2020-03-18 18:33:43 +0100

    co_00: chg

--- co_00_templateToCopy/src/co/Solution.java
+++ co_00_templateToCopy/src/co/Solution.java
@@ -1,9 +1,17 @@
 package co;
 
+import java.util.Arrays;
+
 public class Solution {
 
 	public static int solution(int[] a, int x, int y, int d) {
-		return d;
+		int r = 0;
+		System.out.println("a=" + Arrays.toString(a) + " x=" + x);
+
+		System.out.println("y=" + y + " d=" + d);
+		System.out.println("r=" + r);
+
+		return r;
 	}
 
 	public static int[] solutionArray(int[] a, int x, int y, int d) {
