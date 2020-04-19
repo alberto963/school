63fadec4e042da8faac49f8ac7b34251c50476ba
Alberto <petazzi63@gmail.com>
2020-04-19 22:44:35 +0200

    cj_05: chg

--- cj_05_2019_QR_DatBae/src/co/Solution.java
+++ cj_05_2019_QR_DatBae/src/co/Solution.java
@@ -13,19 +13,22 @@ public class Solution {
 			int n = Integer.parseInt(input.next());
 			int b = Integer.parseInt(input.next());
 			int f = Integer.parseInt(input.next());
-			if (i > 0) {
-				// System.exit(n);
-			}
+
 			// System.out.println("t=" + t + " t=" + t + " n=" + n + " b=" + b + " f=" + f);
 
-			String result = solution(i + 1, n, b, f, input);
+			String result = solution(n, b, f, input);
 			System.out.println(result);
+			int r = Integer.parseInt(input.next());
+
+			if (r != 1) {
+				System.exit(f);
+			}
 		}
 
 		input.close();
 	}
 
-	public static String solution(int i, int n, int b, int f, Scanner input) {
+	public static String solution(int n, int b, int f, Scanner input) {
 
 		// System.out.println(String.format("%2s%s", '0', "1"));
 		// System.out.println(String.format("%02d%s", 0, "1"));
@@ -55,46 +58,48 @@ public class Solution {
 	}
 
 	public static String[] testStore(int n, int f) {
-		// System.out.println("val=" + val);
 
-		int f0 = Math.min(n, (int) Math.pow(2, f));
-		String[] cols = new String[n];
-		int p = n / f0;
-		int q = n / p;
-		// System.out.println("p=" + p);
+		int cols = f;
+		int rows = (int) Math.pow(2, f);
+		char[][] m = new char[rows][f];
 
-		for (int b = 0; b < p; b++) {
-			for (int c = 0; c < f0; c++) {
-				String binaryString = Integer.toBinaryString(c);
+		for (int r = 0; r < rows; r++) {
+			String binaryString = Integer.toBinaryString(r);
+			int l = binaryString.length();
+			String format = l < f ? "%0" + (f - l) + "d%s" : "%s";
+			binaryString = l < f ? String.format(format, 0, binaryString) : binaryString;
+			m[r] = binaryString.toCharArray();
+		}
 
-				int l = binaryString.length();
-				String format = l < f ? "%0" + (f - l) + "d%s" : "%s";
-//			System.out.println("format=" + format);
-//			System.out.println("binaryString=" + binaryString);
-
-				String val = l < f ? String.format(format, 0, binaryString) : binaryString;
-				// System.out.println("val=" + val);
-
-				cols[b * q + c] = val;
+		char[][] mr = new char[cols][rows];
+		for (int r = 0; r < rows; r++) {
+			for (int c = 0; c < cols; c++) {
+				mr[c][r] = m[r][c];
 			}
 		}
-		// System.out.println("cols=" + Arrays.toString(cols));
 
-		String[] testStore = new String[f];
+		int attempts = f;
+		int blocks = n / rows + 1;
+		String[] result = new String[attempts];
+		Arrays.fill(result, "");
+
+		for (int a = 0; a < attempts; a++) {
+			for (int b = 0; b < blocks; b++) {
+				result[a] = result[a] + String.valueOf(mr[a]);
+			}
+		}
+
+		for (int a = 0; a < attempts; a++) {
+			result[a] = result[a].substring(0, n);
+		}
+
+		int maxAttempts = f;
+		String[] testStore = new String[maxAttempts];
 		Arrays.fill(testStore, "");
 
-		for (int b = 0; b < p; b++) {
-			for (int c = 0; c < f0; c++) {
-				for (int r = 0; r < f; r++) {
-
-					int s = f - r - 1;
-					// System.out.println("c=" + c + " r=" + r + " s=" + s);
-
-					testStore[r] = testStore[r] + cols[c].substring(s, s + 1);
-				}
-			}
+		for (int a = 0; a < maxAttempts; a++) {
+			testStore[a] = result[attempts - a - 1];
 		}
-		// System.out.println("testStore=" + Arrays.toString(testStore));
 
 		return testStore;
 	}
@@ -103,32 +108,52 @@ public class Solution {
 		String[] returnedCols = new String[n];
 		Arrays.fill(returnedCols, "");
 
+		// System.out.println("returnedFromStore=" +
+		// Arrays.toString(returnedFromStore));
+
+		int length = returnedFromStore[0].length();
+		// System.out.println("length=" + length);
+
 		for (int c = 0; c < f; c++) {
-			for (int r = 0; r < returnedFromStore[c].length(); r++) {
+			for (int r = 0; r < length; r++) {
 
-				// System.out.println("c=" + c + " r=" + r + " s=" + s);
+				// System.out.println("c=" + c + " r=" + r);
 
-				returnedCols[r] = returnedCols[r] + returnedFromStore[c].substring(r, r + 1);
+				returnedCols[r] = returnedFromStore[c].substring(r, r + 1) + returnedCols[r];
 			}
 		}
 
 		// System.out.println("returnedCols=" + Arrays.toString(returnedCols));
 
-		int[] returnedColsAsInt = new int[returnedFromStore[0].length()];
+		int[] returnedColsAsInt = new int[length];
 
-		for (int c = 0; c < returnedFromStore[0].length(); c++) {
+		for (int c = 0; c < length; c++) {
 			returnedColsAsInt[c] = Integer.parseUnsignedInt(returnedCols[c], 2);
 		}
 
-		// System.out.println("returnedColsAsInt=" +
-		// Arrays.toString(returnedColsAsInt));
+		// System.out.println("returnedColsAsInt=" + Arrays.toString(returnedColsAsInt));
 
+		int rows = (int) Math.pow(2, f);
+
+		// System.out.println("n=" + n + " length=" + length);
+
+		int b = 0;
 		boolean[] mem = new boolean[n];
-		for (int j = 0; j < returnedColsAsInt.length; j++) {
-			if (returnedColsAsInt[j] >= 0) {
-				mem[returnedColsAsInt[j]] = true;
+		for (int j = 0; j < length; j++) {
+			int curr = returnedColsAsInt[j];
+			if (j < length - 1) {
+				int next = returnedColsAsInt[j + 1];
+				mem[b * rows + curr] = true;
+
+				if (curr >= next) {
+					b++;
+				}
+			} else {
+				mem[b * rows + curr] = true;
 			}
 		}
+
+		// System.out.println("mem=" + Arrays.toString(mem));
 
 		String result = "";
 		for (int j = 0; j < n; j++) {
